1eb6836fbc254c2da5426c2263fb65f9c8dc3f13
cagan-kutuk <batincagan06@gmail.com>
2025-12-16 21:53:02 +0300

    Update SayıTahminOyunu.py

--- SayıTahminOyunu.py
+++ SayıTahminOyunu.py
@@ -3,7 +3,7 @@ import random
 
 sayi = random.randint(1, 100)
 
-
+tahmin = None
 
 while tahmin != sayi:
     tahmin = int(input("1 ile 100 arasında bir sayı tahmin et: "))
@@ -14,5 +14,6 @@ while tahmin != sayi:
         print("Aşağı in!")
     else:
         print("Tebrikler, doğru tahmin ettinz!")
+
 
 #None: boş demektir sadece değer atamadığım değişkeni başlatmak için kullandım.   
